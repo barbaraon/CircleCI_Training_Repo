635d155a4db99e46f9bcb46d415d5dc5ccd0fded
Bárbara Neves <barbara.neves.oliveira@gmail.com>
2022-03-31 19:03:01 +0100

    teste2

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -5,6 +5,5 @@ sudo apt install openjdk-11-jdk
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
 
-
-#echo "Running SFDX Scanner"
-#npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
+echo "Running SFDX Scanner"
+npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
